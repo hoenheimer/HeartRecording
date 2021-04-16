52fbe3a115baa11ad2a4645eacddbf78dca178ab
MagicAna <2273703684@qq.com>
2021-04-16 17:44:44 +0800

    录音相关

--- HeartRecording/HeartRecording/Record/RecordViewController.swift
+++ HeartRecording/HeartRecording/Record/RecordViewController.swift
@@ -6,12 +6,20 @@
 //
 
 import UIKit
+import ReactiveCocoa
+import ReactiveSwift
+
 
 class RecordViewController: AnaLargeTitleViewController {
+    var animationView: RippleAnimationView!
     var mainView: UIView!
-    var imageView: UIView!
+    var heartImageView: UIImageView!
     var label: UILabel!
+    var buttonBackgroundView: UIView!
+    var buttonGradient: CAGradientLayer!
     var button: UIButton!
+    
+    let manager = RecordManager()
     
     
     override func configure() {
@@ -19,11 +27,78 @@ class RecordViewController: AnaLargeTitleViewController {
         
         setTitle(title: "HeartRecording")
         
+        animationView = RippleAnimationView(bgColor: .color(hexString: "#FF8282"))
+        contentView.addSubview(animationView)
         
+        mainView = UIView()
+        mainView.layer.cornerRadius = 134
+        mainView.backgroundColor = .color(hexString: "#FF8282")
+        contentView.addSubview(mainView)
+        
+        heartImageView = UIImageView()
+        heartImageView.backgroundColor = .white
+        mainView.addSubview(heartImageView)
+        
+        label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.text = "When you have found your baby‘s heartbeat, tap the button to record now"
+        label.textColor = .white
+        label.font = UIFont(name: "Poppins-Light", size: 14)
+        mainView.addSubview(label)
+        
+        buttonBackgroundView = UIView()
+        buttonBackgroundView.backgroundColor = .clear
+        buttonBackgroundView.layer.cornerRadius = 24
+        buttonBackgroundView.layer.borderWidth = 1
+        buttonBackgroundView.layer.borderColor = UIColor.color(hexString: "#80FCFCFC").cgColor
+        contentView.addSubview(buttonBackgroundView)
+        
+        buttonGradient = CAGradientLayer()
+        buttonGradient.colors = [UIColor.color(hexString: "#FF7474").cgColor, UIColor.color(hexString: "#FF4747").cgColor]
+        buttonGradient.startPoint = CGPoint(x: 0.5, y: 0)
+        buttonGradient.endPoint = CGPoint(x: 0.5, y: 1)
+        buttonGradient.cornerRadius = 24
+        buttonBackgroundView.layer.addSublayer(buttonGradient)
+        
+        buttonBackgroundView.setShadow(color: .color(hexString: "#28A0A3BD"), offset: CGSize(width: 0, height: 16), radius: 24)
+        
+        button = UIButton()
+        button.setTitle("Start Recording", for: .normal)
+        button.setTitleColor(.color(hexString: "#FCFCFC"), for: .normal)
+        button.titleLabel?.font = UIFont(name: "Poppins-SemiBold", size: 16)
+        button.reactive.controlEvents(.touchUpInside).observeValues {
+            [weak self] button in
+            guard let self = self else { return }
+            if !self.manager.isRecording {
+                self.manager.beginRecord()
+                if self.manager.isRecording {
+                    button.setTitle("Done", for: .normal)
+                }
+            } else {
+                self.manager.stopRecord()
+                if !self.manager.isRecording {
+                    button.setTitle("Start Recording", for: .normal)
+                }
+            }
+        }
+        buttonBackgroundView.addSubview(button)
     }
     
     
     override func layoutContentView() -> CGFloat {
-        return 0
+        mainView.bounds = CGRect(origin: .zero, size: CGSize(width: 268, height: 268))
+        mainView.center = CGPoint(x: view.halfWidth(), y: 98 + mainView.halfHeight())
+        animationView.frame = mainView.frame
+        heartImageView.bounds = CGRect(origin: .zero, size: CGSize(width: 43, height: 40))
+        heartImageView.center = CGPoint(x: mainView.halfWidth(), y: 65 + heartImageView.halfHeight())
+        let size = label.sizeThatFits(CGSize(width: 229, height: CGFloat.greatestFiniteMagnitude))
+        label.bounds = CGRect(origin: .zero, size: size)
+        label.center = CGPoint(x: mainView.halfWidth(), y: heartImageView.maxY() + 28 + label.halfHeight())
+        buttonBackgroundView.bounds = CGRect(origin: .zero, size: CGSize(width: 175, height: 48))
+        buttonBackgroundView.center = CGPoint(x: view.halfWidth(), y: mainView.maxY() + 127 + buttonBackgroundView.halfHeight())
+        buttonGradient.frame = buttonBackgroundView.bounds
+        button.frame = buttonBackgroundView.bounds
+        return buttonBackgroundView.maxY()
     }
 }
---
+++ HeartRecording/HeartRecording/Tools/RecordManager.swift
@@ -0,0 +1,84 @@
+//
+//  RecordManager.swift
+//  HeartRecording
+//
+//  Created by MagicAna on 2021/4/16.
+//
+
+import Foundation
+import AVFoundation
+import ReactiveSwift
+import ReactiveCocoa
+
+
+class RecordManager {
+    var recorder: AVAudioRecorder?
+    let file_path: String? = "/Users/apple/Desktop/test.wav"//NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first?.appending("/record.wav")
+    var isRecording = false
+    let needPermissionPipe = Signal<Int, Never>.pipe()
+    
+    
+    //开始录音
+    func beginRecord() {
+        let session = AVAudioSession.sharedInstance()
+        //设置session类型
+        do {
+            try session.setCategory(AVAudioSession.Category.playAndRecord)
+        } catch let err{
+            print("设置类型失败:\(err.localizedDescription)")
+        }
+        //设置session动作
+        do {
+            try session.setActive(true)
+        } catch let err {
+            print("初始化动作失败:\(err.localizedDescription)")
+        }
+        var canRecord = true
+        session.requestRecordPermission {
+            [weak self] granted in
+            guard let self = self else { return }
+            if !granted {
+                canRecord = false
+                self.needPermissionPipe.input.send(value: 1)
+            }
+        }
+        if !canRecord {
+            return
+        }
+        //录音设置，注意，后面需要转换成NSNumber，如果不转换，你会发现，无法录制音频文件，我猜测是因为底层还是用OC写的原因
+        let recordSetting: [String: Any] = [AVSampleRateKey: NSNumber(value: 16000),//采样率
+                                            AVFormatIDKey: NSNumber(value: kAudioFormatLinearPCM),//音频格式
+                                            AVLinearPCMBitDepthKey: NSNumber(value: 16),//采样位数
+                                            AVNumberOfChannelsKey: NSNumber(value: 1),//通道数
+                                            AVEncoderAudioQualityKey: NSNumber(value: AVAudioQuality.min.rawValue)//录音质量
+        ];
+        //开始录音
+        do {
+            let url = URL(fileURLWithPath: file_path!)
+            recorder = try AVAudioRecorder(url: url, settings: recordSetting)
+            recorder!.prepareToRecord()
+            recorder!.record()
+            isRecording = true
+            print("开始录音")
+        } catch let err {
+            print("录音失败:\(err.localizedDescription)")
+        }
+    }
+    
+    
+    //结束录音
+    func stopRecord() {
+        if let recorder = self.recorder {
+            if recorder.isRecording {
+                print("正在录音，马上结束它，文件保存到了：\(file_path!)")
+            }else {
+                print("没有录音，但是依然结束它")
+            }
+            recorder.stop()
+            self.recorder = nil
+        } else {
+            print("没有初始化")
+        }
+        isRecording = false
+    }
+}
